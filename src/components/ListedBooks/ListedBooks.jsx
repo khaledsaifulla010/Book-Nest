084220886bfbe2a793b41840db32c029b69bcd56
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-03 20:28:53 +0600

    added sorting using no of pages

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -8,10 +8,13 @@ import {
 } from "../../utility/AddToDB/AddToDB";
 import ReadBookCard from "../ReadBookCard/ReadBookCard";
 import WishBookCard from "../WishBookCard/WishBookCard";
+import { IoIosArrowDropdown } from "react-icons/io";
 
 const ListedBooks = () => {
   const [readList, setReadList] = useState([]);
   const [wishList, setWishList] = useState([]);
+
+  const [sort, setSort] = useState("");
 
   const allBooks = useLoaderData();
   // console.log(allBooks);
@@ -40,6 +43,17 @@ const ListedBooks = () => {
     setWishList(readWishList);
   }, []);
 
+  const handleSortType = (sortType) => {
+    setSort(sortType);
+
+    if (sortType === "No of Pages") {
+      const sortedReadListByNoOfPages = [...readList].sort(
+        (a, b) => b.totalPages - a.totalPages
+      );
+      setReadList(sortedReadListByNoOfPages);
+    }
+  };
+
   return (
     <div className="mt-12">
       <Tabs className={"text-xl font-black"}>
@@ -49,9 +63,32 @@ const ListedBooks = () => {
         </TabList>
 
         <TabPanel>
-          <h2 className="text-4xl font-bold mt-8 ml-12 mb-12">
-            Total Read Books : {readList.length}
-          </h2>
+          <div className="flex items-center justify-between px-12 mt-12">
+            <h2 className="text-4xl font-bold">
+              Total Read Books : {readList.length}
+            </h2>
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="border-2 p-2 rounded-xl bg-green-500 w-auto  flex items-center gap-2  font-black text-white text-lg"
+              >
+                {sort ? `Sort By : ${sort}` : "Sort By"}
+                <IoIosArrowDropdown></IoIosArrowDropdown>
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52  p-2 shadow border-2 "
+              >
+                <li onClick={() => handleSortType("Rating")}>
+                  <a>Ratings</a>
+                </li>
+                <li onClick={() => handleSortType("No of Pages")}>
+                  <a>No of Pages</a>
+                </li>
+              </ul>
+            </div>
+          </div>
 
           {readList.map((book) => (
             <ReadBookCard key={book.bookId} book={book}></ReadBookCard>
